d92a800ebe05afb1930b0fcd9bfd9283d56addff
RamyaTrinapps <ramya.k@tringapps.com>
2017-12-26 04:47:52 -0800

    Achieved full screen view of HLS video

--- app/src/main/java/com/example/tringappsadmin/exoplayer_sample/ExoPlayerActivity.java
+++ app/src/main/java/com/example/tringappsadmin/exoplayer_sample/ExoPlayerActivity.java
@@ -1,9 +1,5 @@
 package com.example.tringappsadmin.exoplayer_sample;
 
-/**
- * Created by Tringapps Admin on 12/19/2017.
- */
-
 import android.annotation.SuppressLint;
 import android.media.MediaCodec;
 import android.media.MediaPlayer;
@@ -25,6 +21,7 @@ import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.RendererCapabilities;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.decoder.DecoderCounters;
@@ -32,13 +29,18 @@ import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.extractor.ExtractorsFactory;
 import com.google.android.exoplayer2.source.LoopingMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroup;
 import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.source.chunk.MediaChunk;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.text.Cue;
+import com.google.android.exoplayer2.text.TextRenderer;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectorResult;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
@@ -46,21 +48,10 @@ import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 import com.google.android.exoplayer2.video.VideoRendererEventListener;
 
-
-
-/*
-Created by: Ayal Fieldust
-Date: 8/2017
-
-Description:
-This Example app was created to show a simple example of ExoPlayer Version 2.5.1.
-There is an option to play mp4 files or live stream content.
-Exoplayer provides options to play many different formats, so the code can easily be tweaked to play the requested format.
-scroll down to "ADJUST HERE:" I & II to change between sources.
- */
+import java.util.List;
 
 public class ExoPlayerActivity extends AppCompatActivity implements VideoRendererEventListener, View.OnClickListener,
-        SimpleExoPlayer.VideoListener{
+        SimpleExoPlayer.VideoListener, TextRenderer.Output {
 
 
     private static final String TAG = "MainActivity";
@@ -68,7 +59,7 @@ public class ExoPlayerActivity extends AppCompatActivity implements VideoRendere
     private SimpleExoPlayer player;
     private TextView resolutionTextView;
     private Button smallScreen, fullScreen;
-    private int video_width=0 , video_height=0;
+    private int video_width = 0, video_height = 0;
 
 
     @Override
@@ -79,70 +70,37 @@ public class ExoPlayerActivity extends AppCompatActivity implements VideoRendere
         resolutionTextView = (TextView) findViewById(R.id.textView);
 
 
-        smallScreen = (Button)findViewById(R.id.small_screen);
+        smallScreen = (Button) findViewById(R.id.small_screen);
         fullScreen = (Button) findViewById(R.id.full_screen);
 
         smallScreen.setOnClickListener(this);
         fullScreen.setOnClickListener(this);
 
-// 1. Create a default TrackSelector
         BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
         TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
         TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
 
-// 2. Create a default LoadControl
         LoadControl loadControl = new DefaultLoadControl();
 
-// 3. Create the player
         player = ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
         simpleExoPlayerView = new SimpleExoPlayerView(this);
         simpleExoPlayerView = (SimpleExoPlayerView) findViewById(R.id.exoplayerView);
 
-//Set media controller
         simpleExoPlayerView.setUseController(true);
         simpleExoPlayerView.requestFocus();
 
-// Bind the player to the view.
         simpleExoPlayerView.setPlayer(player);
 
 
-// I. ADJUST HERE:
-//CHOOSE CONTENT: LiveStream / SdCard
+        Uri mp4VideoUri = Uri.parse("https://nbclim-f.akamaihd.net/i/Prod/NBCU_LM_VMS_-_KNTV/924/799/Celebration_of_Life_Service_Honors_Late_SF_Mayor_Ed_Lee__797116.mp4,,.csmil/master.m3u8"); //Radnom 540p indian channel
 
-//LIVE STREAM SOURCE: * Livestream links may be out of date so find any m3u8 files online and replace:
-
-//        Uri mp4VideoUri =Uri.parse("http://81.7.13.162/hls/ss1/index.m3u8"); //random 720p source
-        Uri mp4VideoUri =Uri.parse("https://nbclim-f.akamaihd.net/i/Prod/NBCU_LM_VMS_-_KNTV/924/799/Celebration_of_Life_Service_Honors_Late_SF_Mayor_Ed_Lee__797116.mp4,,.csmil/master.m3u8"); //Radnom 540p indian channel
-//        Uri mp4VideoUri =Uri.parse("FIND A WORKING LINK ABD PLUg INTO HERE"); //PLUG INTO HERE<------------------------------------------
-
-
-//VIDEO FROM SD CARD: (2 steps. set up file and path, then change videoSource to get the file)
-//        String urimp4 = "path/FileName.mp4"; //upload file to device and add path/name.mp4
-//        Uri mp4VideoUri = Uri.parse(Environment.getExternalStorageDirectory().getAbsolutePath()+urimp4);
-
-
-
-
-
-//Measures bandwidth during playback. Can be null if not required.
         DefaultBandwidthMeter bandwidthMeterA = new DefaultBandwidthMeter();
-//Produces DataSource instances through which media data is loaded.
         DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "exoplayer2example"), bandwidthMeterA);
-//Produces Extractor instances for parsing the media data.
         ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
 
-
-// II. ADJUST HERE:
-
-//This is the MediaSource representing the media to be played:
-//FOR SD CARD SOURCE:
-//        MediaSource videoSource = new ExtractorMediaSource(mp4VideoUri, dataSourceFactory, extractorsFactory, null, null);
-
-//FOR LIVESTREAM LINK:
         MediaSource videoSource = new HlsMediaSource(mp4VideoUri, dataSourceFactory, 1, null, null);
         final LoopingMediaSource loopingSource = new LoopingMediaSource(videoSource);
 
-// Prepare the player with the source.
         player.prepare(loopingSource);
 
         player.addListener(new ExoPlayer.EventListener() {
@@ -158,7 +116,7 @@ public class ExoPlayerActivity extends AppCompatActivity implements VideoRendere
 
             @Override
             public void onLoadingChanged(boolean isLoading) {
-                Log.v(TAG, "Listener-onLoadingChanged...isLoading:"+isLoading);
+                Log.v(TAG, "Listener-onLoadingChanged...isLoading:" + isLoading);
             }
 
             @Override
@@ -219,22 +177,22 @@ public class ExoPlayerActivity extends AppCompatActivity implements VideoRendere
     public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
         video_width = width;
         video_height = height;
-        Log.v(TAG, "onVideoSizeChanged ["  + " width: " + width + " height: " + height + "]");
-        resolutionTextView.setText("RES:(WxH):"+width+"X"+height +"\n           "+height+"p");
+        Log.v(TAG, "onVideoSizeChanged [" + " width: " + width + " height: " + height + "]");
+        resolutionTextView.setText("RES:(WxH):" + width + "X" + height + "\n           " + height + "p");
 
-        if (simpleExoPlayerView.getLayoutParams().width==624){
+        /*if (simpleExoPlayerView.getLayoutParams().width==624){
             simpleExoPlayerView.getLayoutParams().height = 352;
             simpleExoPlayerView.getLayoutParams().width = 352;
-        }else{
-            simpleExoPlayerView.getLayoutParams().height = 352;
-            simpleExoPlayerView.getLayoutParams().width = 624;
-        }
+        }else{*/
+            /*simpleExoPlayerView.getLayoutParams().height = 352;
+            simpleExoPlayerView.getLayoutParams().width = 624;*/
+//        }
     }
 
     @Override
     public void onRenderedFirstFrame() {
-        simpleExoPlayerView.getLayoutParams().height = 352;
-        simpleExoPlayerView.getLayoutParams().width = 624;
+//        simpleExoPlayerView.getLayoutParams().height = 400;
+//        simpleExoPlayerView.getLayoutParams().width = 624;
     }
 
     @Override
@@ -247,7 +205,7 @@ public class ExoPlayerActivity extends AppCompatActivity implements VideoRendere
 
     }
 
-   /* @Override
+    @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
@@ -259,7 +217,7 @@ public class ExoPlayerActivity extends AppCompatActivity implements VideoRendere
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
-    }*/
+    }
 
     @SuppressLint("InlinedApi")
     private void hideSystemUi() {
@@ -270,8 +228,6 @@ public class ExoPlayerActivity extends AppCompatActivity implements VideoRendere
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
-
-
 
 
 //-------------------------------------------------------ANDROID LIFECYCLE---------------------------------------------------------------------------------------------
@@ -291,7 +247,7 @@ public class ExoPlayerActivity extends AppCompatActivity implements VideoRendere
     @Override
     protected void onResume() {
         super.onResume();
-//        hideSystemUi();
+        hideSystemUi();
         Log.v(TAG, "onResume()...");
     }
 
@@ -311,8 +267,8 @@ public class ExoPlayerActivity extends AppCompatActivity implements VideoRendere
     @Override
     public void onClick(View view) {
 
-        switch (view.getId()){
-            case R.id.small_screen:
+        switch (view.getId()) {
+           /* case R.id.small_screen:
 
                 simpleExoPlayerView.getLayoutParams().height = 352;
 
@@ -326,9 +282,41 @@ public class ExoPlayerActivity extends AppCompatActivity implements VideoRendere
 
                 simpleExoPlayerView.getLayoutParams().height = 352;
                 simpleExoPlayerView.getLayoutParams().width = 624;
-
+*/
         }
     }
+
+    @Override
+    public void onCues(List<Cue> cues) {
+       /* if (subtitleView != null) {
+            subtitleView.onCues(cues);
+        }*/
+    }
+
 }
+
+    /*public  class MappingTrackSelector
+            extends TrackSelector{
+        MappingTrackSelector.MappedTrackInfo mappedTrackInfo = trackSelector.getCurrentMappedTrackInfo();
+        TrackGroupArray textGroups = mappedTrackInfo.getTrackGroups(TRACK_TEXT); // list of captions
+        int groupIndex = 1; // index of desired caption track within the textGroups array
+
+        trackSelector.setRendererDisabled(TRACK_TEXT, false);
+        MappingTrackSelector.SelectionOverride override =
+                new MappingTrackSelector.SelectionOverride(fixedFactory, groupIndex, 0);
+        trackSelector.setSelectionOverride(TRACK_TEXT, textGroups, override);
+
+
+        @Override
+        public TrackSelectorResult selectTracks(RendererCapabilities[] rendererCapabilities, TrackGroupArray trackGroups) throws ExoPlaybackException {
+            return null;
+        }
+
+        @Override
+        public void onSelectionActivated(Object info) {
+
+        }
+    }*/
+
 
 
